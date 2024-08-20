e198c2781da0a76b17b70d042a78f362bb9cd1c7
APIVerve Team <hello@apiverve.com>
2024-08-20 11:12:25 -0500

    Bug Fixes

--- TriviaGeneratorAPIClient.cs
+++ TriviaGeneratorAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, triviaQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, TriviaGeneratorQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(triviaQueryOptions options = null)
+        public ResponseObj Execute(TriviaGeneratorQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(triviaQueryOptions options)
+        private string constructURL(TriviaGeneratorQueryOptions options)
         {
             string url = _apiEndpoint;
 
